f826c9f0619efc5587d026de98a8a5076bf3619b
Jacob Birkett <spikespaz@outlook.com>
2023-07-31 21:05:28 -0700

    flake: make systems extensible

--- flake.nix
+++ flake.nix
@@ -4,20 +4,27 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    # See <https://github.com/nix-systems/nix-systems>.
+    systems.url = "github:nix-systems/default";
+
     devshell.url = "github:numtide/devshell";
+    devshell.inputs.systems.follows = "systems";
+
     nixfmt.url = "github:serokell/nixfmt";
+    nixfmt.inputs.flake-utils.inputs.systems.follows = "systems";
+
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
   };
 
-  outputs = { self, nixpkgs, devshell, nixfmt, ... }:
+  outputs = { self, nixpkgs, systems, devshell, nixfmt, ... }:
     let
       lib = nixpkgs.lib.extend (import ./nix/lib.nix);
-      systems = [ "x86_64-linux" ];
-      pkgsFor =
-        lib.genAttrs systems (system: import nixpkgs { inherit system; });
+      eachSystem = lib.genAttrs (import systems);
+      pkgsFor = eachSystem (system: import nixpkgs { inherit system; });
       packageDate = (lib.mkDate (self.lastModifiedDate or "19700101")) + "_"
         + (self.shortRev or "dirty");
     in {
@@ -27,12 +34,12 @@
         };
       };
 
-      packages = lib.genAttrs systems (system:
+      packages = eachSystem (system:
         (self.overlays.default pkgsFor.${system} pkgsFor.${system}) // {
           default = self.packages.${system}.waybar;
         });
 
-      devShells = lib.genAttrs systems (system:
+      devShells = eachSystem (system:
         let
           pkgs = import nixpkgs {
             inherit system;
@@ -40,7 +47,6 @@
           };
         in { default = pkgs.callPackage ./nix/devshell.nix { }; });
 
-      formatter =
-        lib.genAttrs systems (system: nixfmt.packages.${system}.default);
+      formatter = eachSystem (system: nixfmt.packages.${system}.default);
     };
 }
